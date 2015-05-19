d3efde70bcbf5205f88d6ea2083ecb573745a06e
Jared <jaredtaylor@cbrc.net>
2015-05-19 09:17:23 -0400

    Initial Commit

---
+++ Tables/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tables
+//
+//  Created by Jared Taylor on 5/19/15.
+//  Copyright (c) 2015 Jared Taylor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tables/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tables
+//
+//  Created by Jared Taylor on 5/19/15.
+//  Copyright (c) 2015 Jared Taylor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
