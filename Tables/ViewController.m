96b5e1c81e771f7858dc04702e9720a997adb476
Jared <jaredtaylor@cbrc.net>
2015-05-19 10:04:12 -0400

    created a table with a row and sections

--- Tables/ViewController.m
+++ Tables/ViewController.m
@@ -12,6 +12,8 @@
 
 @end
 
+NSString* CELL_NAME= @"Cell";
+
 @implementation ViewController
 
 - (void)viewDidLoad {
@@ -22,6 +24,23 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(NSInteger) numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
+}
+
+-(NSInteger) tableView: (UITableView *)tableView numberOfRowsInSection: (NSInteger)section{
+    return 10;
+}
+
+-(UITableViewCell*) tableView: (UITableView*)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:CELL_NAME];
+    if (cell == nil){
+        cell = [[UITableViewCell alloc] initWithStyle: UITableViewCellStyleDefault reuseIdentifier:CELL_NAME];
+    }
+    [[cell textLabel] setText: @"Hello World"];
+    return cell;
 }
 
 @end
